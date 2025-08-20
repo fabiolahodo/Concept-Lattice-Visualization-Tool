4e80ce817ed235275250da2a625868d7d8573599
fabiolahodo <83943427+fabiolahodo@users.noreply.github.com>
2025-08-20 12:50:59 +0200

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,6 @@
 // rollup.config.js
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/views/explorer.js',
@@ -8,5 +9,15 @@ export default {
     format: 'esm',
     sourcemap: true
   },
-  plugins: [resolve()]
+  plugins: [resolve({
+      browser: true,
+      preferBuiltins: false,  // important for browser/Electron renderer
+    })],
+
+  // Optional: silence D3 circular warnings
+  onwarn(warning, warn) {
+    if (warning.code === 'CIRCULAR_DEPENDENCY' && /d3-selection/.test(warning.message)) return;
+    warn(warning);
+  },
+  
 };
